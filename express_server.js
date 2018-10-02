16adfdb0792b629f8efa3c98dd7ac6716d37afd2
johnbxu <johnbxu@gmail.com>
2018-10-02 15:40:33 +0000

    added urls_new get request to server routes

--- express_server.js
+++ express_server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
+const generateRandomString = require('./generateRandomString');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
-var urlDatabase = {
+// variables
+let urlDatabase = {
   'b2xVn2': 'http://lighthouselabs.ca',
   '9sm5xk': 'http://www.google.com',
 };
 
-// app.get('/', (req, res) => {
-  // res.send('Hello!');
-// });
-
+// get requests
 app.get('/', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get('/urls/new', (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render('urls_new', templateVars);
 });
 
 app.get('/urls.json', (req, res) => {
@@ -35,6 +41,14 @@ app.get('/hello', (req, res) => {
   res.render('hello_world', templateVars);
 });
 
+// post requests
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('ok');
+})
+
+
+// listen
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
